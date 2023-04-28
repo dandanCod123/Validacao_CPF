01ab088707b649534e1c371dcc4a59b9acb31ded
dandanCod123 <danielmouradesousa5@gmail.com>
2023-04-28 16:27:42 -0300

    validação 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useState } from 'react';
+import axios from 'axios';
 
-function App() {
+import '../src/App.css';
+
+interface Props {}
+
+const ValidarCPF: React.FC<Props> = () => {
+  const [valido, setValido] = useState<string>('');
+  const inputRef = useRef<HTMLInputElement>(null);
+
+  const validarCPF = async () => {
+    const cpf = inputRef.current?.value;
+    const token =  "eyJraWQiOiJyc2ExIiwiYWxnIjoiUlMyNTYifQ.eyJzdWIiOiIyMDgyMTkyMjQ1OSIsImF1ZCI6Im10Y2lkYWRhby5tdC5nb3YuYnIiLCJzY29wZSI6WyJlbWFpbCIsIm9wZW5pZCIsInBob25lIiwicHJvZmlsZSJdLCJhbXIiOiJwYXNzd2QiLCJpc3MiOiJodHRwczpcL1wvc3NvLnN0YWdpbmcuYWNlc3NvLmdvdi5iclwvIiwiZXhwIjoxNTczNjczMTMxLCJpYXQiOjE1NzM2Njk1MzEsImp0aSI6ImE0ZDkwNGEyLTRhNDMtNDJiMy04NmQ3LWVhZWQ1Y2I1M2MyZiJ9.FH0Al-J2cIrFMNNki5QT8IzEhR-hQheAA7A02mvgTtpmTbAVxQWyGw-JuFVdycf9kTXUB9O3PUbzzGr5uq4aUWHYefEHR2jqhl4jq1Xa80lggu40djXQLl3DAVySML6KWTFUwbVLIsxbxaQ6Jh3mX4y069xScXzOtPUDGPgnZSZroc3LqdzjbaDqHSksrCp6XqMJxR5AYTDMi_M2yonXiBiKFfiP06d1bq11AVeEiyX5qOsV2mokbckFgT7UO2dPDRAG1KF-ko0jN-20TC0YWX--jjQp_Y3dRu4oexuTmBdJASt9k7YYhKlRM4AWnX89VmjLX6_CoXKzXSxqRZIRKg";
+    const idCliente = "123";
+    const url = "https://api.staging.acesso.gov.br";
+
+    const response = await axios.get(url, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+        'X-CLIENT-ID': idCliente,
+      },
+    });
+
+    setValido(response.data.situacao);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+    <h1>VALIDE O CPF</h1>
+      <input type="text" ref={inputRef} />
+      <button onClick={validarCPF}>Validar CPF</button>        
+      
+      <p>{valido}</p>
     </div>
   );
-}
+};
 
-export default App;
+export default ValidarCPF;
